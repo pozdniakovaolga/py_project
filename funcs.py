af1404c4cd41eca928b096a7ee8787927b6491d6
pozdniakova olga <caramelca07@mail.ru>
2023-04-20 13:25:49 +0200

    added diff function

--- funcs.py
+++ funcs.py
@@ -2,3 +2,9 @@ def func_summ(number1, number2):
     """Функция, которая складывает два числа"""
 
     return number1 + number2
+
+
+def func_diff(number1, number2):
+    """Функция, которая вычитает number2 из number1"""
+
+    return number1 - number2
